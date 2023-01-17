a1ad2a590186ac44af8c197affc75ad6bd8ee183
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2023-01-17 12:12:26 +0100

    first commit

---
+++ src/components/Job.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import IconButton from "@mui/material/IconButton";
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+const Job = () => {
+    const [isLiked, setIsLiked] = useState(false)
+    function toggleIsLike() {
+        setIsLiked(!isLiked);
+      }
+    return (
+        <div>
+            <div className='job-container'>
+                <h3 className='job-title'>Assistant ressources humaines</h3>
+                <div className='tags-container'>
+                    <h3 className='contrat-type'><LibraryBooksIcon/>CDD</h3>
+                    <h3 className='localisation'><LocationOnIcon />Strasbourg</h3>
+                    <h3 className='date-de-poste'><CalendarTodayIcon/>Il y a 2j</h3>
+                </div>
+                <IconButton className='like-button' onClick={() => toggleIsLike()} >
+                {isLiked ? <FavoriteBorderIcon /> : <FavoriteIcon />}
+                </IconButton>
+            </div>
+        </div>
+    );
+};
+
+export default Job;
---
+++ src/components/Modal.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import "./modal.css"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Job from './Job';
+
+const Modal = () => {
+    const [modal, setModal] = useState(false)
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
+    return (
+        <div>
+            <button onClick={toggleModal} className='btn-modal'>Open</button>
+            {modal && (
+                      <div className='modal'>
+                        <div onClick={toggleModal} className='overlay'></div>
+                            <div className='modal-content'>
+                                <div className='reseaux-icons'>
+                                    <div className='linkedin-logo'></div>
+                                    <div className='twitter-logo'></div>
+                                    <div className='facebook-logo'></div>
+                                    <div className='instagram-logo'></div>
+                                </div>
+                                <div className='background-img'></div>
+                                <div className='logo-entreprise'></div>
+                                <div className='presentation-part'>
+                                    <h2 className='title'>Presentation</h2>
+                                    <p className='content'>TestTestLoremLorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam 
+                                    quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?
+                                    </p>
+                                </div>
+                                <div className='jobs-dispo-part'>
+                                <h2 className='jobs-title'>X-jobs Disponible</h2>
+                                <div className='jobcontainer'>
+                                    <Job />
+                                    <Job />
+                                    <Job />
+                                    <Job />
+                                    <Job />
+                                    <Job />
+                                    <Job />
+                                </div>
+
+                                </div>
+                                <div className='profils-recherche-part'>
+                                    <h2 className='title'>Profils recherché</h2>
+                                    <p className='content'>TestTestLoremLorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam 
+                                    quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?  quas, excepturi ipsam amet dignissimos incidunt rem cumque,
+                                    laborum quam a minima aliquid praesentium molestiae nam cupiditate aspernatur,
+                                    nihil alias laboriosam?
+                                    </p>
+                                </div>
+                                <button onClick={toggleModal} className='close-modal'><ArrowBackIcon /></button>
+                        </div>
+                      </div>
+            )}
+        </div>
+    );
+};
+
+export default Modal;
